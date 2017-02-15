13d34e646e36754e795540e8b56c28a4bb514da8
Aliaksei Parkhimovich <aliaksei_parkhimovich@epam.com>
2017-02-15 15:45:39 +0300

    start

---
+++ LLW_Framework/JournalPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace LLW_Framework
+{
+    public class JournalPage
+    {
+        internal IWebDriver driver;
+
+        public JournalPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+
+    }
+}
---
+++ LLW_Framework/JournalTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLW_Framework
+{
+    [TestFixture]
+    public class JournalTest
+    {
+        internal IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void a()
+        {
+            driver.Navigate().GoToUrl(SetUpData.BaseUrl + "aacr");
+            JournalPage jp = new JournalPage(driver);
+            //driver.FindElement(By.XPath("//span[contains(text(), /aacr/subjects )]"));
+            //bool isElementPresent = driver.FindElement(By.XPath("//span[contains(text(), 'Subjects' )]")).Enabled;
+            bool isElementPresent = driver.FindElement(By.XPath("//a[contains(text(), 'Articles & Issues')]")).Enabled;
+            Assert.True(isElementPresent);
+
+        }
+
+        [OneTimeTearDown]
+        public void CloseDriver()
+        {
+            driver.Dispose();
+        }
+    }
+}
